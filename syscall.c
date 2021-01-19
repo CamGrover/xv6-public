568e4282df7f0bc58a710ea517201540f0af8164
Cameron Grover <camdgrover@gmail.com>
2021-01-19 13:13:11 -0600

    Changes sys_settickets() and sys_getpinfo() parameters to void

--- syscall.c
+++ syscall.c
@@ -104,8 +104,8 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
-extern int sys_settickets(int);
-extern int sys_getpinfo(struct pstat*);
+extern int sys_settickets(void);
+extern int sys_getpinfo(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
